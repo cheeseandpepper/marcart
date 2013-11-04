b659ec44fbd7bb3700f0c0b2f21279ec8ed4189d
Mike Lerner <cheeseandpepper@gmail.com>
2013-11-04 16:22:04 -0500

    Initialize repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marcart::Application.config.secret_key_base = '205e9009b94658c7bd9980cb3093fcf0bfbc0741f6633a5c368416b59a1c09f9fb191a2d4eb321e03fa0268b5511920e2e5e1a070da90d9aebf9942abc8a6c3d'
